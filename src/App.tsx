18bb867750df0588e81efad1b3146ac9c7a5ddf0
Kumar Gaurav <kumar@make.io>
2026-05-15 16:34:48 +0530

    video added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import ProductViewer from "./components/ProductViewer";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
+import Showcase from "./components/Showcase";
 
 gsap.registerPlugin(ScrollTrigger);
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Hero/>
       <ProductViewer/>
+      <Showcase/>
     </main>
   );
 }
---
+++ src/components/Showcase.tsx
@@ -0,0 +1,63 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { memo, type FC } from "react";
+import { useMediaQuery } from "react-responsive";
+
+interface ShowcaseProps {}
+
+const Showcase: FC<ShowcaseProps> = () => {
+    const isTablet = useMediaQuery({query : '(max-width :1024px)'});
+    useGSAP(()=>{
+        if(!isTablet){
+            const timeline = gsap.timeline({
+                scrollTrigger: {
+                    trigger : "#showcase",
+                    start : 'top top',
+                    end : 'bottom top',
+                    scrub : true
+                }
+            })
+            timeline.to('.mask img',{
+                transform : 'scale(1.1)'
+            }).to('.content', {opacity :1 ,y:0, ease:'power1.in'})
+        }
+    },[])
+  return (
+    <section id="showcase">
+      <div className="media">
+        <video src="/videos/game.mp4" autoPlay loop playsInline muted />
+        <div className="mask">
+          <img src="/mask-logo.svg" />
+        </div>
+      </div>
+      <div className="content">
+        <div className="wrapper">
+          <div className="lg:max-w-md">
+            <h2>Rocket Chip</h2>
+            <div className="space-mt-5 y-5 pe-10">
+              <p>
+                Introducing{" "}
+                <span className="text-white">
+                  M4- the next generation of m4 power
+                </span>
+                .M4 Powers
+              </p>
+              <p>
+                Our most advanced series of chips ever built for a pro laptop.
+                Each chip in the M5 family delivers phenomenal single- and
+                multithreaded CPU performance and faster unified
+              </p>
+              <p>
+                The SSD storage is up to twice as fast as before.13 And with
+                powerful Neural Accelerators, you can fly through AI tasks at
+                mind-bending speeds.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default memo(Showcase);
